c5019d2938e07ee7bfd4ede6286c1f7f9b1dc3b3
ann ngatia <annngatia94@gmail.com>
2025-11-13 18:54:37 +0300

    Added page: Doctors

--- clinic-appointment/src/App.jsx
+++ clinic-appointment/src/App.jsx
@@ -1,8 +1,9 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import Patients from "./pages/Patients"
-import Appointments from "./pages/Appointments"
-import "./styles/app.css"
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Patients from "./pages/Patients";
+import Appointments from "./pages/Appointments";
+import Doctors from "./pages/Doctors";
+import "./styles/app.css";
 
 function App() {
   return (
@@ -10,16 +11,20 @@ function App() {
       <nav>
         <Link to="/">Home</Link>{" | "}
         <Link to="/patients">Patients</Link>{" | "}
-        <Link to="/appointments">Appointments</Link>
+        <Link to="/appointments">Appointments</Link>{" | "}
+        <Link to="/doctors">Doctors</Link>
       </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/patients" element={<Patients />} />
-        <Route path="/appointments" element={<Appointments />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/patients" element={<Patients />} />
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/doctors" element={<Doctors />} />
+        </Routes>
+      </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
