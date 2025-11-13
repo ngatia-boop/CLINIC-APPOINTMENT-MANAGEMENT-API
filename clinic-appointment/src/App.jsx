8eaa104a1494a538638756d91d79a4daa8dec225
ann ngatia <annngatia94@gmail.com>
2025-11-13 18:54:37 +0300

    Added page: Doctors

--- clinic-appointment/src/App.jsx
+++ clinic-appointment/src/App.jsx
@@ -1,23 +1,27 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
-import Home from "./pages/Home"
-import Patients from "./pages/Patients"
-import Appointments from "./pages/Appointments"
-import "./styles/app.css"
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './pages/Home'
+import Patients from './pages/Patients'
+import Appointments from './pages/Appointments'
+import Doctors from './pages/Doctors'
 
 function App() {
   return (
     <Router>
       <nav>
-        <Link to="/">Home</Link>{" | "}
-        <Link to="/patients">Patients</Link>{" | "}
+        <Link to="/">Home</Link>
+        <Link to="/patients">Patients</Link>
         <Link to="/appointments">Appointments</Link>
+        <Link to="/doctors">Doctors</Link>
       </nav>
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/patients" element={<Patients />} />
-        <Route path="/appointments" element={<Appointments />} />
-      </Routes>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/patients" element={<Patients />} />
+          <Route path="/appointments" element={<Appointments />} />
+          <Route path="/doctors" element={<Doctors />} />
+        </Routes>
+      </div>
     </Router>
   )
 }
