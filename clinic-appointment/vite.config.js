d2e6258694f32fb2d7140cc4e0881d0bcd4133cf
ann ngatia <annngatia94@gmail.com>
2025-11-14 13:48:59 +0300

    resolve merge conflicts

--- clinic-appointment/vite.config.js
+++ clinic-appointment/vite.config.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -5,3 +6,17 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 })
+=======
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  server: {
+    proxy: {
+      'api': 'http://localhost:5555'
+    }
+  },
+  plugins: [react()],
+})
+>>>>>>> e043172 (Added pages: Home, Patients, Appointments)
