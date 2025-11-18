6cf3f246ead578668d23367c69b602f8b6f577d8
ann ngatia <annngatia94@gmail.com>
2025-11-18 19:03:20 +0300

    modify main.py

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,7 @@ from backend import create_app
 from flask_cors import CORS
 
 app = create_app()
-
-# Enable CORS for your frontend
 CORS(app, origins=["http://localhost:5173"])
 
 if __name__ == "__main__":
-    # Run on port 5555 to match your old server
     app.run(host="127.0.0.1", port=5555, debug=True)
