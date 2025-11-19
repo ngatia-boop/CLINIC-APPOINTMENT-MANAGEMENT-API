615e557ba1bdfb2285cb5690a6853ec78719e2d3
ann ngatia <annngatia94@gmail.com>
2025-11-19 12:55:57 +0300

    updates

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,7 @@ from backend import create_app
 from flask_cors import CORS
 
 app = create_app()
-CORS(app, origins=["http://localhost:5173"])
+CORS(app, origins=["https://clinic-appointment-management-api-v.vercel.app/"])
 
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5555, debug=True)
