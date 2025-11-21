587283d6561b034ecda2312f3c7f28dced7c04f1
ann ngatia <annngatia94@gmail.com>
2025-11-21 19:11:32 +0300

    updates

--- backend/build.sh
+++ backend/build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
+echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
+
+echo "Starting app with Gunicorn..."
+exec gunicorn main:app --bind 0.0.0.0:$PORT
